c9954ea648ad8bed20773a65d4b9bf929cedf74c
Marlon Tavares <marlontf@gmail.com>
2022-07-13 19:42:09 -0300

    Revert "Teste Docker"
    This reverts commit 6c6ed40b1e79ac4eecef778bf80d01cbebf6bfff.

--- www/index.php
+++ www/index.php
@@ -1,56 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-   <meta charset="UTF-8">
-   <title>Lista de tarefas</title>
-</head>
-<!-- Fonts Awesome -->
-<script src="https://kit.fontawesome.com/53cccf545a.js" crossorigin="anonymous"></script>
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<link rel="stylesheet" href="assets/css/style.css">
-<body>
-   <div class="container">
-      <div class="title">
-         <h3>Lista de tarefas
-            <span style="float:right; padding:1px; ">
-               <i class="fas fa-plus-square"></i>
-            </span>
-         </h3>
-         
-      </div>
-      <div class="item">
-         Fazer compras adsfasdf asdaasfasdfa  sdfa  asdfasdfas asfas sadf asdfsdfasdd
-         <span class="controls">
-            <i class="fas fa-pen-square"></i>
-            <i class="fas fa-minus-square"></i>
-         </span>
-      </div>
-      <div class="item">
-         Fazer compras
-         <span class="controls">
-            <i class="fas fa-pen-square"></i>
-            <i class="fas fa-minus-square"></i>
-         </span>
-      </div>
-      <div class="item">
-         Fazer compras
-         <span class="controls">
-            <i class="fas fa-pen-square"></i>
-            <i class="fas fa-minus-square"></i>
-         </span>
-      </div>
-   </div>
-   <script src="assets/js/scripts.js"></script>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            body {
+                font-family: "Arial", sans-serif;
+                font-size: larger;
+            }
 
-
-
-<!-- 
-<?php
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+        <?php
         $connection = new PDO('mysql:host=mariadb;dbname=demo;charset=utf8', 'root', 'root');
         $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'demo'");
         $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
@@ -65,5 +34,6 @@
             }
             echo '</ul>';
         }
-        ?> -->
-
+        ?>
+    </body>
+</html>
